a96ac695fd90a21d839741003cb4b01d1f349811
b2023644 <b2023644@my.shu.ac.uk>
2015-02-04 17:38:28 +0000

    uptotask5

--- StudentEnrol/grails-app/conf/BootStrap.groovy
+++ StudentEnrol/grails-app/conf/BootStrap.groovy
@@ -3,11 +3,25 @@ import com.studentenrol.*
 class BootStrap {
 
     def init = { servletContext ->
+
+		def leader1=new Leader(fullName:'Dr Anna Roberts',
+					post:'Senior Lecturer',
+					subject:'Animation',
+					email:'a.d.roberts@theUni.ac.uk',
+					office:'Room 1234',
+					bio:'''jksdfkjdsf ''').save()
+		
+		def lect1 = new Lecturer( name:'Dr Steve Crossbar',
+					     post:'Lecturer',
+					     email:'s.h.crossbar@theUni.ac.uk',
+					     department:'Computing',
+					     subject:'Web Design',
+					     research:'Knowledge Management').save()
 		
 		def course1= new Course( title:'Interactive Media with Animation',
 					department:'Computing',
 					description:''' lorem ipsum....''',
-					leader:'Joe Jojo',
+				leader:Leader.findByFullName('Dr Anna Roberts'),
 					code: 'IMA101',
 					startDate: new Date('19/01/2015'),
 					endDate: new Date('19/01/2020'))
@@ -15,8 +29,13 @@ class BootStrap {
 	if(!course1.save()){
 			course1.errors.allErrors.each{ error ->
 		println "An error occurred whilst creating course1:${error}"
+
+		
+
 		}
     }
+
+	course1.addToLecturers(lect1)
     }
     def destroy = {
     }
